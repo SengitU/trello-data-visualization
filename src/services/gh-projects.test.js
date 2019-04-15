01c75bc53a3491867f6a90c969ad8acbbb82ae75
Markus Heilig <markusheilig@gmx.net>
2019-04-15 15:11:25 +0200

    cleanup/reformat tests

--- src/services/gh-projects.test.js
+++ src/services/gh-projects.test.js
@@ -3,79 +3,52 @@ import { listNames } from '../board-information/lists';
 
 describe('github projects', () => {
     test('it should return all issues with status "done"', () => {
-        const issue1 = {
-            number: 1,
-            labels: ['tdd']
-        };
-        const issue2 = {
-            number: 2,
-            labels: ['communication']
-        };
-        const issue3 = {
-            number: 3,
-            labels: ['agile-methods']
-        };
+        const issue1 = { number: 1, labels: [{ name: 'tdd' }] };
+        const issue2 = { number: 2, labels: [{ name: 'communication' }] };
+        const issue3 = { number: 3, labels: [{ name: 'agile-methods' }] };
+        const allIssues = [issue1, issue2, issue3];
 
-        const issues = [issue1, issue2, issue3];
-    
-        const cards = [
-            {
-                content_url: 'https://github.hc.ag/api/v3/repos/mheilig/apprenticeship/issues/1'
-            },
-            {
-                content_url: 'https://github.hc.ag/api/v3/repos/mheilig/apprenticeship/issues/3'
-            }
+        const doneCards = [
+            { content_url: 'https://www.github.com/api/v3/repos/mheilig/apprenticeship/issues/1' },
+            { content_url: 'https://www.github.com/api/v3/repos/mheilig/apprenticeship/issues/3' }
         ];
-        
-        expect(getDoneIssues(issues, cards)).toEqual([issue1, issue3]);
+
+        expect(getDoneIssues(allIssues, doneCards)).toEqual([issue1, issue3]);
     })
 
-    test('it should not return done issues without a label', () => {
-        const issue1 = {
-            number: 1,
-            labels: []
-        };
-        const issue2 = {
-            number: 2,
-            labels: ['agile-methods']
-        };
-        const issues = [issue1, issue2];
+    test('it should ignore issues without a label', () => {
+        const issueWithoutLabel = { number: 1, labels: [] };
+        const issueWithOneLabel = { number: 2, labels: ['agile-methods'] };
+        const issueWithMultipleLabels = { number: 3, labels: ['tdd', 'communication'] };
+        const issues = [issueWithoutLabel, issueWithOneLabel, issueWithMultipleLabels];
 
-        const cards = [
-            {
-                content_url: 'https://github.hc.ag/api/v3/repos/mheilig/apprenticeship/issues/1'
-            },
-            {
-                content_url: 'https://github.hc.ag/api/v3/repos/mheilig/apprenticeship/issues/2'
-            }
+        const doneCards = [
+            { content_url: 'https://www.github.com/api/v3/repos/mheilig/apprenticeship/issues/1' },
+            { content_url: 'https://www.github.com/api/v3/repos/mheilig/apprenticeship/issues/2' },
+            { content_url: 'https://www.github.com/api/v3/repos/mheilig/apprenticeship/issues/3' }
         ];
-        
-        expect(getDoneIssues(issues, cards)).toEqual([issue2]);
+
+        expect(getDoneIssues(issues, doneCards)).toEqual([issueWithOneLabel, issueWithMultipleLabels]);
     })
 
     test('it should extract the issue number from a card', () => {
-        const card = {
-            content_url: 'https://github.hc.ag/api/v3/repos/mheilig/apprenticeship/issues/17'
-        };
+        const card = { content_url: 'https://www.github.com/api/v3/repos/mheilig/apprenticeship/issues/17' };
         expect(extractIssueNumber(card)).toBe(17);
     })
 
     test('it should return all issues from column "Done" which have at least one label', () => {
-        const issueFetcher = () => {
-            return Promise.resolve([
-                { number: 1, name: 'Issue #1', labels: [] },
-                { number: 2, name: 'Issue #2', labels: [{ name: 'tdd', id: 12 }] },
-                { number: 3, name: 'Issue #3', labels: [{ name: 'agile-methods', id: 15 }] },
-                { number: 4, name: 'Issue #4', labels: [{ name: 'communication', id: 20 }] },
-            ]);
-        };
-        const doneCardFetcher = () => {
-            return Promise.resolve([
-                { content_url: 'https://github.com/api/v3/repos/mheilig/apprenticeship/issues/1' },
-                { content_url: 'https://github.com/api/v3/repos/mheilig/apprenticeship/issues/2' },
-                { content_url: 'https://github.com/api/v3/repos/mheilig/apprenticeship/issues/3' },
-            ]);
-        };
+        const issueFetcher = () => Promise.resolve([
+            { number: 1, name: 'Issue #1', labels: [] },
+            { number: 2, name: 'Issue #2', labels: [{ name: 'tdd', id: 12 }] },
+            { number: 3, name: 'Issue #3', labels: [{ name: 'agile-methods', id: 15 }] },
+            { number: 4, name: 'Issue #4', labels: [{ name: 'communication', id: 20 }] },
+        ]);
+
+        const doneCardFetcher = () => Promise.resolve([
+            { content_url: 'https://github.com/api/v3/repos/mheilig/apprenticeship/issues/1' },
+            { content_url: 'https://github.com/api/v3/repos/mheilig/apprenticeship/issues/2' },
+            { content_url: 'https://github.com/api/v3/repos/mheilig/apprenticeship/issues/3' },
+        ]);
 
         process.env.REACT_APP_GITHUB_ID_DONE_COLUMN = 123;
 
@@ -83,12 +56,7 @@ describe('github projects', () => {
         getDoneGithubIssues(issueFetcher, doneCardFetcher)
             .then(issuesInTrelloFormat => {
                 expect(issuesInTrelloFormat).toEqual({
-                    lists: [
-                        {
-                            name: listNames.DONE_OVERALL,
-                            id: process.env.REACT_APP_GITHUB_ID_DONE_COLUMN
-                        }
-                    ],
+                    lists: [{ name: listNames.DONE_OVERALL, id: process.env.REACT_APP_GITHUB_ID_DONE_COLUMN }],
                     cards: [
                         {
                             idList: process.env.REACT_APP_GITHUB_ID_DONE_COLUMN,
